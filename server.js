6758519b3b7c0a3a9c44bacd990714ea3a75ffba
omarbinsaleh <omarbinsaleh44@gmail.com>
2025-05-02 20:44:25 +0400

    added functionality to connect to mongoDB database

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const path = require('path')
+const path = require('path');
+const connectDB = require('./config/db');
 
 const app = express();
 const PORT = process.env.SERVER_PORT || 5000;
@@ -13,6 +14,9 @@ app.use(cors({
    allowedHeaders: ["Content-Type", "Authorization"]
 }));
 app.use(express.json());
+
+// Connect to DB
+connectDB();
 
 // Routes
 app.get('/', (req, res) => {
